1a2cc7a44a46479267416368eb804940a04dc193
Cody Benkoski <cob322@lehigh.edu>
2020-08-17 22:27:10 -0400

    its alive

--- js/drawsensors.js
+++ js/drawsensors.js
@@ -1,33 +1,53 @@
-const consistent_highlight = []
-const MARKERS = {}
+const consistent_highlight = [];
+const MARKERS = {};
+const CIRCLE_MARKERS = {};
+const PULSERS = {};
+const RELATIVE_VOLUME = {};
 var grouped_data;
+const MEDIAN_SPL = 31.63172027543478;
+const MEDIAN_MOD = 120;
+const MEDIAN_PULSE = 5;
+const RAINBOW = new Rainbow();
+RAINBOW.setNumberRange(21,132);
+RAINBOW.setSpectrum("#bed8ec", "#125ca4"); //https://vega.github.io/vega/docs/schemes/
+
+// https://stackoverflow.com/questions/37115491/how-to-set-volume-of-audio-object
 
 
 function loadData(map) {
-	$.getJSON("simple.json", function(data){
+	$.getJSON("grouped.json", function(data){
 		const lat_lng_set = new Set()
 		$.each(data, function(){
-			const {name, latitude, longitude, avg_db} = this;
-			var color = "#004BBD";
-			if(avg_db < 20) {
-				color = "#238823"
-			} else if(avg_db < 35) {
-				color = "#007000";
-			} 
-			const pos_str = latitude+","+longitude
-			if(!lat_lng_set.has(pos_str)) {
-				lat_lng_set.add(pos_str)
-			} else {
-				return 1;
-			}
-			console.log(this);
-			MARKERS[name] = L.circleMarker([latitude, longitude], {color: color }).bindPopup(name+"<br>Decibels: "+avg_db).addTo(map);
+			$.each(this, function(){
+				const {name, latitude, longitude, avg_db, modified_db} = this;
+				const color = RAINBOW.colourAt(modified_db);
+				const pulse_ratio = modified_db/MEDIAN_MOD;
+				const bias = (pulse_ratio <= 0) ? 1.5 : -3.5;
+				const pulse_rate = MEDIAN_PULSE + (bias * pulse_ratio**2);
+				const pos_str = latitude+","+longitude
+				if(!lat_lng_set.has(pos_str)) {
+					lat_lng_set.add(pos_str)
+				} else {
+					return 1;
+				}
+				// console.log(this);
+				const pulse_boi = L.icon.pulse({iconSize:[20,20],color: "#"+color});
+				// console.log(pulse_boi);
+				PULSERS[name] = pulse_boi;
+				const [__,_, uniq_class] = (pulse_boi.options.className).split(" ");
+				// console.log(uniq_class);
+				MARKERS[name] = L.marker([latitude, longitude], {icon:pulse_boi, opacity:1}).addTo(map);
+				$("."+uniq_class).css("animation-duration", ""+pulse_rate+"s");
+				console.log($("."+uniq_class).css("animation-duration"));
+				CIRCLE_MARKERS[name] = L.circleMarker([latitude, longitude], {color: "#"+color}).bindPopup(name+"<br>Decibels: "+avg_db+"<br>Relative volume: "+modified_db+"%").addTo(map);
+				RELATIVE_VOLUME[name] = modified_db;
+			});
 		});
 	});
 }
 
 
-const map = L.map('county-map').setView([40.657052, -74.038430], 12);
+const map = L.map('county-map').setView([40.657052, -74.038430], 18);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/bencodyoski/ck7v6s6vf06sl1jpt9lnqpoqx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmVuY29keW9za2kiLCJhIjoiY2s1c2s0Y2JmMHA2bzNrbzZ5djJ3bDdscyJ9.7MuHmoSKO5zAgY0IKChI8w', {
 	maxZoom: 18,
--- rearrange_by_day.py
+++ rearrange_by_day.py
@@ -2,7 +2,8 @@ import json
 import dateparser
 from collections import OrderedDict
 from datetime import datetime
-from statistics import mean
+from statistics import mean, median
+from math import floor
 def sort_fxn(dt):
     time_str = dt[0]
     p = dateparser.parse(str(time_str))
@@ -12,21 +13,26 @@ def sort_fxn(dt):
     return float(datetime.timestamp(p))
 max_num_sounds = 0
 max_ts = None
+db_list = []
+MEDIAN = 31.63172027543478
 with open("annotations.json", "r") as sound_file:
     sound_dict = json.load(sound_file)
     # date_set = set()
     dates = dict()
     for sound in sound_dict.copy().values():
         ts = sound.get("human_ts")
-        if ts == "2018-01-08 08:00:00":
-            print()
-            print(sound)
-            print()
         db_ = sound.get("decibels")
         if (ts is None) or (db_ is None):
             # print(sound)
             continue
-        sound["avg_db"] = mean(db_)
+        avg_db = mean(db_)
+        sound["avg_db"] = avg_db
+        ratio = avg_db/MEDIAN
+        rel_vol = floor(ratio*100)
+        bias = 10 if rel_vol >= 100 else -10
+        penalty = (ratio**2) * bias
+        sound["modified_db"] = floor(rel_vol + penalty)
+        db_list .append(sound["modified_db"])
         # h = dateparser.parse(str(ts))
         # if h is None:
         #     print(ts)
@@ -38,15 +44,18 @@ with open("annotations.json", "r") as sound_file:
                 max_num_sounds = len(dates[ts])
                 max_ts = ts
     # dates.sort(key=lambda dt: dateparser.parse(dt)) 
-    with open("unsorted1.json", "w") as uns:
+    with open("unsorted.json", "w") as uns:
         json.dump(obj=dates, fp=uns)
+    print(max(db_list))
+    print(median(db_list))
+    print(min(db_list))
     sorted_dates = OrderedDict()
     for k,v in sorted(dates.items(), key=sort_fxn):
         if k == "2018-01-08 08:00:00":
             if v[0].get("decibels") is None:
                 print(v[0])
         sorted_dates[k] = v
-    with open("grouped1.json", "w") as group:
+    with open("grouped.json", "w") as group:
         json.dump(obj=sorted_dates, fp=group)
     k = dates.keys()
     with open("buggy.json", "w") as b:
